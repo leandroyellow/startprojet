e70d112c4f95667e41609c051891d31eeaa48a9f
leandro notebook <leandroqueiroztrepador@gmail.com>
2026-06-28 16:33:32 -0300

    feat: add new text home

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       Se você esta lendo isso é porque a pessoa que te mandou te ama e te quer
-      bem!!! Jesus te ama! teste
+      bem!!! Jesus te ama!
     </h1>
   );
 }
